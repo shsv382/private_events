ef40024e616aeb9a83287f28786213c2b3ffbad6
shsv382 <shsv382@gmail.com>
2017-09-09 21:11:58 +0400

    Added events scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,11 @@ class Event < ApplicationRecord
 	validates :name, presence: true
 	validates :date, presence: true
 	validates :location, presence: true
+
 	belongs_to :creator, class_name: "User"
 	has_many :event_attendings, foreign_key: "attended_event_id"
 	has_many :attendees, through: :event_attendings, source: :event_attendee
+
+	scope :past, 	 -> { where("date <  ?", Time.now).order(date: :desc) }
+	scope :upcoming, -> { where("date >= ?", Time.now).order(date: :desc) }
 end
